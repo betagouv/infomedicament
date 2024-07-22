2efde78d9321dc83718850b778595a479ce0651d
Maud Royer <hello@maudroyer.fr>
2024-07-22 13:34:12 +0200

    feat: affichage basique avec skip sur les erreurs

--- src/app/medicament/[CIS]/page.tsx
+++ src/app/medicament/[CIS]/page.tsx
@@ -1,9 +1,18 @@
 import { cache } from "react";
 import { fr } from "@codegouvfr/react-dsfr";
 
-import { db } from "@/database";
+import {
+  db,
+  PresInfoTarif,
+  SpecComposant,
+  SpecDelivrance,
+  SpecElement,
+  Specialite,
+  SubstanceNom,
+} from "@/database";
 
 import liste_CIS_MVP from "./liste_CIS_MVP.json";
+import Tag from "@codegouvfr/react-dsfr/Tag";
 
 export async function generateStaticParams(): Promise<{ CIS: string }[]> {
   return liste_CIS_MVP.map((CIS) => ({
@@ -12,11 +21,61 @@ export async function generateStaticParams(): Promise<{ CIS: string }[]> {
 }
 
 const getSpecialite = cache(async (CIS: string) => {
-  return await db
+  const specialite: Specialite = await db
     .selectFrom("Specialite")
     .where("SpecId", "=", CIS)
     .selectAll()
     .executeTakeFirstOrThrow();
+
+  const elements: SpecElement[] = await db
+    .selectFrom("Element")
+    .where("SpecId", "=", CIS)
+    .selectAll()
+    .execute();
+
+  const composants: Array<SpecComposant & SubstanceNom> = (
+    await Promise.all(
+      elements.map((el) =>
+        db
+          .selectFrom("Composant")
+          .where((eb) =>
+            eb.and([eb("SpecId", "=", CIS), eb("ElmtNum", "=", el.ElmtNum)]),
+          )
+          .innerJoin("Subs_Nom", "Composant.NomId", "Subs_Nom.NomId")
+          .selectAll()
+          .execute(),
+      ),
+    )
+  ).flat();
+
+  const prix: PresInfoTarif[] = await db
+    .selectFrom("Presentation")
+    .where("SpecId", "=", CIS)
+    .innerJoin(
+      "CNAM_InfoTarif",
+      "Presentation.codeCIP13",
+      "CNAM_InfoTarif.Cip13",
+    )
+    .selectAll("CNAM_InfoTarif")
+    .execute();
+
+  const delivrance: SpecDelivrance[] = await db
+    .selectFrom("Spec_Delivrance")
+    .where("SpecId", "=", CIS)
+    .innerJoin(
+      "DicoDelivrance",
+      "Spec_Delivrance.DelivId",
+      "DicoDelivrance.DelivId",
+    )
+    .selectAll()
+    .execute();
+
+  return {
+    specialite,
+    composants,
+    prix,
+    delivrance,
+  };
 });
 
 export default async function Home({
@@ -24,7 +83,7 @@ export default async function Home({
 }: {
   params: { CIS: string };
 }) {
-  const specialite = await getSpecialite(CIS);
+  const { specialite, composants, prix, delivrance } = await getSpecialite(CIS);
 
   const denom = specialite.SpecDenom01.split(" ")
     .map((word) =>
@@ -32,5 +91,42 @@ export default async function Home({
     )
     .join(" ");
 
-  return <h1 className={fr.cx("fr-h2")}>{denom}</h1>;
+  return (
+    <>
+      <h1 className={fr.cx("fr-h2")}>{denom}</h1>
+      <p>
+        {specialite.SpecGeneId ? (
+          <Tag
+            small
+            iconId="fr-icon-capsule-fill"
+            nativeButtonProps={{
+              className: fr.cx("fr-tag--green-emeraude"),
+            }}
+          >
+            Générique
+          </Tag>
+        ) : null}{" "}
+        {delivrance.find((d) => d.DelivCourt.startsWith("liste")) ? (
+          <Tag
+            small
+            iconId="fr-icon-file-text-fill"
+            nativeButtonProps={{
+              className: fr.cx("fr-tag--green-archipel"),
+            }}
+          >
+            Sur ordonnance
+          </Tag>
+        ) : null}
+      </p>
+      {prix.length ? (
+        <p>
+          <b>Prix</b> <Tag small>{prix[0]?.Prix} €</Tag>{" "}
+          <Tag small>{prix[0].Taux}</Tag>
+        </p>
+      ) : null}
+      <p>
+        <b>Substance active</b> {composants.map((c) => c.NomLib).join(", ")}
+      </p>
+    </>
+  );
 }
--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,13 @@ import { createPool } from "mysql2";
 
 interface Database {
   Specialite: SpecialiteTable;
+  Element: SpecElementTable;
+  Composant: SpecComposantTable;
+  Subs_Nom: Subs_NomTable;
+  Presentation: PresentationTable;
+  CNAM_InfoTarif: InfoTarifTable;
+  Spec_Delivrance: SpecDelivranceTable;
+  DicoDelivrance: DicoDelivranceTable;
 }
 
 interface SpecialiteTable {
@@ -27,7 +34,64 @@ interface SpecialiteTable {
   Een: string;
 }
 
+interface SpecElementTable {
+  SpecId: string;
+  ElmtNum: string;
+  ElmtNom: string;
+  ElmtRem: string;
+  ElmtRefDosage: string;
+}
+
+interface SpecComposantTable {
+  SpecId: string;
+  ElmtNum: string;
+  NatuId: string;
+  CompNum: number;
+  SubstId: string;
+  NomId: string;
+  CompDosage: string;
+  CompRem: string;
+}
+
+interface Subs_NomTable {
+  SubsId: string;
+  NomId: string;
+  NomLib: string;
+}
+
+interface PresentationTable {
+  SpecId: string;
+  PresNum: string;
+  PresNom01: string;
+  codeCIP13: string;
+}
+
+interface InfoTarifTable {
+  Cip13: string;
+  Taux: string;
+  Prix: string;
+  DateEffet: Date;
+  IndicRestreinte: string;
+}
+
+interface SpecDelivranceTable {
+  SpecId: string;
+  DelivId: string;
+}
+
+interface DicoDelivranceTable {
+  DelivId: string;
+  DelivCourt: string;
+  DelivLong: string;
+}
+
 export type Specialite = Selectable<SpecialiteTable>;
+export type SpecElement = Selectable<SpecElementTable>;
+export type SpecComposant = Selectable<SpecComposantTable>;
+export type SubstanceNom = Selectable<Subs_NomTable>;
+export type PresInfoTarif = Selectable<InfoTarifTable>;
+export type SpecDelivrance = Selectable<SpecDelivranceTable> &
+  Selectable<DicoDelivranceTable>;
 
 export const db = new Kysely<Database>({
   dialect: new MysqlDialect({
