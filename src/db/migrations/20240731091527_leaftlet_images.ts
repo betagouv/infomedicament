b70f2621d005f575ec75652e2faedf3a2d3a89fa
Maud Royer <hello@maudroyer.fr>
2024-08-01 10:35:42 +0200

    fix: use text not varchar
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/db/migrations/20240731091527_leaftlet_images.ts
+++ src/db/migrations/20240731091527_leaftlet_images.ts
@@ -3,7 +3,7 @@ import type { Kysely } from "kysely";
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("leaflet_images")
-    .addColumn("path", "varchar(255)", (col) => col.primaryKey())
+    .addColumn("path", "text", (col) => col.primaryKey())
     .addColumn("image", "bytea")
     .execute();
 }
