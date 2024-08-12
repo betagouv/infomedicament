771168ab82c4c24173b34a5cfbdce8e39c6546ae
Maud Royer <hello@maudroyer.fr>
2024-08-12 12:12:26 +0200

    feat: display search results
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/app/rechercher/page.tsx
+++ src/app/rechercher/page.tsx
@@ -3,16 +3,38 @@ import Input from "@codegouvfr/react-dsfr/Input";
 import { pdbmMySQL, Specialite } from "@/db/pdbmMySQL";
 import { fr } from "@codegouvfr/react-dsfr";
 
+import { formatSpecName } from "@/formatUtils";
+import liste_CIS_MVP from "../medicament/[CIS]/liste_CIS_MVP.json";
+import Link from "next/link";
+
 async function getResults(query: string) {
-  const specialites: Specialite[] = await pdbmMySQL
-    .selectFrom("Specialite")
-    .where("SpecDenom01", "like", `%${query}%`)
-    .selectAll()
-    .execute();
+  const specialites: Specialite[] = (
+    await pdbmMySQL
+      .selectFrom("Specialite")
+      .where("SpecDenom01", "like", `%${query}%`)
+      .selectAll()
+      .execute()
+  ).filter((specialite) => liste_CIS_MVP.includes(specialite.SpecId));
 
   return {
     specialites,
   };
+}
+
+function groupSpecialites(
+  specialites: Specialite[],
+): Map<string, Specialite[]> {
+  const groups = new Map<string, Specialite[]>();
+  for (const specialite of specialites) {
+    const regexMatch = specialite.SpecDenom01.match(/^[^0-9]+/);
+    const groupName = regexMatch ? regexMatch[0] : specialite.SpecDenom01;
+    if (groups.has(groupName)) {
+      groups.get(groupName)?.push(specialite);
+    } else {
+      groups.set(groupName, [specialite]);
+    }
+  }
+  return groups;
 }
 
 export default async function Page({
@@ -37,7 +59,29 @@ export default async function Page({
           }}
         />
       </form>
-      {results && <span>{results.specialites.length} RÉSULTATS</span>}
+      {results && (
+        <>
+          <p>{results.specialites.length} RÉSULTATS</p>
+          <ul>
+            {Array.from(groupSpecialites(results.specialites).entries()).map(
+              ([groupName, specialites]: [string, Specialite[]]) => (
+                <li key={groupName} className={"fr-mb-2w"}>
+                  {formatSpecName(groupName)}
+                  <ul>
+                    {specialites?.map((specialite) => (
+                      <li key={specialite.SpecId}>
+                        <Link href={`/medicament/${specialite.SpecId}`}>
+                          {formatSpecName(specialite.SpecDenom01)}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ),
+            )}
+          </ul>
+        </>
+      )}
     </>
   );
 }
