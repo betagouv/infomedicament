4210daefb89376f6cf94cf19c88a031adcacf49e
Maud Royer <hello@maudroyer.fr>
2024-09-06 13:02:08 +0200

    feat: setup sentry
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -7,6 +9,30 @@ const nextConfig = {
     });
     return config;
   },
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
-export default nextConfig;
+/**
+ * The following environment variables are required in order for Sentry to work
+ * and for the source maps to be uploaded correctly:
+ * - SENTRY_DSN
+ * - SENTRY_URL
+ * - SENTRY_ORG
+ * - SENTRY_PROJECT
+ * - SENTRY_AUTH_TOKEN
+ */
+export default withSentryConfig(nextConfig, {
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+});
