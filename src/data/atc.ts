11e58c313d411635765506fc2d2e89a3514beb84
Maud Royer <hello@maudroyer.fr>
2024-10-14 17:43:37 +0200

    feat: article pages
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/data/atc.ts
+++ src/data/atc.ts
@@ -1,33 +1,22 @@
 import "server-only";
 import atcOfficialLabels from "@/data/ATC 2024 02 15.json";
-
-async function getGristTableData(tableId: string) {
-  const response = await fetch(
-    `https://grist.numerique.gouv.fr/api/docs/${process.env.GRIST_DOC_ID}/tables/${tableId}/records`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.GRIST_API_KEY}`,
-      },
-    },
-  );
-  return await response.json();
-}
+import { getGristTableData } from "@/data/grist";
 
 async function getAtcLabel1(code: string): Promise<string> {
   const data = await getGristTableData("Table_Niveau_1");
-  const record = data.records.find(
+  const record = data.find(
     (record: any) => record.fields.Lettre_1_ATC_1 === code.slice(0, 1),
   );
   if (!record) {
     throw new Error(`ATC code not found: ${code.slice(0, 1)}`);
   }
 
-  return record.fields.Libelles_niveau_1;
+  return record.fields.Libelles_niveau_1 as string;
 }
 
 async function getAtcLabel2(code: string): Promise<string> {
   const atcData = await getGristTableData("Table_Niveau_2");
-  const record = atcData.records.find(
+  const record = atcData.find(
     (record: any) => record.fields.Lettre_2_ATC2 === code.slice(0, 3),
   );
   if (!record) {
@@ -36,15 +25,13 @@ async function getAtcLabel2(code: string): Promise<string> {
 
   const libeleId = record.fields.Libelles_niveau_2;
   const libeleData = await getGristTableData("Intitules_possibles");
-  const libeleRecord = libeleData.records.find(
-    (record: any) => record.id === libeleId,
-  );
+  const libeleRecord = libeleData.find((record: any) => record.id === libeleId);
 
   if (!libeleRecord) {
     throw new Error(`ATC code not found: ${code.slice(0, 3)}`);
   }
 
-  return libeleRecord.fields.Libelles_niveau_2;
+  return libeleRecord.fields.Libelles_niveau_2 as string;
 }
 
 async function getAtcOfficialLabel(code: string): Promise<string> {
