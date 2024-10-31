379f42b3f7d813b76043902f9a329f4d929cbaa3
Maud Royer <hello@maudroyer.fr>
2024-10-31 14:02:47 +0100

    fix: start hotjar with useeffect
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/app/StartHotjar.tsx
+++ src/app/StartHotjar.tsx
@@ -1,14 +1,17 @@
 "use client";
 import Hotjar from "@hotjar/browser";
+import { useEffect } from "react";
 
 const siteId: `${number}` | undefined = process.env
   .NEXT_PUBLIC_HOTJAR_SITE_ID as `${number}` | undefined;
 const hotjarVersion = 6;
 
-if (siteId) {
-  Hotjar.init(Number(siteId), hotjarVersion);
-}
-
 export function StartHotjar() {
+  useEffect(() => {
+    if (siteId) {
+      Hotjar.init(Number(siteId), hotjarVersion);
+    }
+  });
+
   return null;
 }
