d73238445c1f25617c7fe3556d0ddbd74eedf430
Maud Royer <hello@maudroyer.fr>
2025-01-23 16:18:38 +0100

    feat: changes on homepage
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,32 @@ export default async function Page() {
         <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
           <div className={fr.cx("fr-col-12", "fr-col-lg-9", "fr-col-md-10")}>
             <form action="/rechercher">
-              <h1 className={fr.cx("fr-h4")}>
-                Comprenez tout sur vos médicaments
+              <h1
+                className={fr.cx("fr-h2")}
+                style={{
+                  /* Forces 'les' to be on second line */
+                  maxWidth: "47rem",
+                }}
+              >
+                Des réponses fiables à toutes vos questions sur les médicaments
               </h1>
+              <p className={fr.cx("fr-text--md")}>
+                Trouvez instantanément les informations claires, précises et
+                officielles sur vos médicaments, en toute simplicité&nbsp;!
+              </p>
               <AutocompleteSearch inputName="s" className={fr.cx("fr-mb-2w")} />
-              <p className="fr-text--xs">
-                <em>
-                  Vous pouvez chercher un médicament, une molécule (ex&nbsp;:
-                  paracétamol), une pathologie (ex&nbsp;: anxiété) ou un type de
-                  médicaments (ex&nbsp;: antibiotique, contraceptif...)
-                </em>
+              <p className="fr-text--sm">
+                <em>Exemples d&apos;utilisation&nbsp;:</em>
+                <br />
+                Vous cherchez <b>la notice</b> d’un médicament.
+                <br />
+                Vous voulez connaître tous les médicaments qui contiennent la
+                même <b>substance active</b> (ex : paracétamol).
+                <br />
+                Vous voulez savoir quels sont les médicaments prescrits pour une{" "}
+                <b>pathologie</b> (ex : diabète).
+                <br /> Vous voulez tout comprendre d’une{" "}
+                <b>classe de médicaments</b> (ex : antibiotiques).
               </p>
             </form>
           </div>
@@ -67,6 +83,11 @@ export default async function Page() {
                   </Link>
                 </li>
               ))}
+              <li key={"all"}>
+                <Link href="/articles" className={fr.cx("fr-link")}>
+                  Voir tous les articles
+                </Link>
+              </li>
             </ul>
           </div>
           <div
