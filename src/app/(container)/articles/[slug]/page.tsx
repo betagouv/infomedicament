f4c8003f787769d1c265960a55e5d01c7398efb7
Maud Royer <hello@maudroyer.fr>
2025-01-24 15:20:45 +0100

    feat: articles responsive layout + images
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/app/(container)/articles/[slug]/page.tsx
+++ src/app/(container)/articles/[slug]/page.tsx
@@ -3,10 +3,13 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { getArticles } from "@/data/grist/articles";
 import Breadcrumb from "@codegouvfr/react-dsfr/Breadcrumb";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 import { Metadata, ResolvingMetadata } from "next";
 
-export const dynamic = "error";
-export const dynamicParams = true;
+export async function generateStaticParams() {
+  const articles = await getArticles();
+  return articles.map(({ slug }) => ({ params: { slug } }));
+}
 
 async function getArticle(slug: string) {
   const articles = await getArticles();
@@ -38,7 +41,7 @@ export default async function Page(props0: {
 }) {
   const { slug } = await props0.params;
 
-  const { title, source, content } = await getArticle(slug);
+  const { title, source, content, image } = await getArticle(slug);
   return (
     <>
       <Breadcrumb
@@ -52,9 +55,32 @@ export default async function Page(props0: {
         currentPageLabel={title}
       />
       <div className={fr.cx("fr-grid-row")}>
-        <div className={fr.cx("fr-col-md-8")}>
-          <h1 className={fr.cx("fr-mb-2w")}>{title}</h1>
-          <p className={fr.cx("fr-text--light")}>Source&nbsp;: {source}</p>
+        <div className={fr.cx("fr-col-12", "fr-col-md-10", "fr-col-lg-9")}>
+          <h1 className={fr.cx("fr-mb-1w")}>{title}</h1>
+          <p className={fr.cx("fr-text--light", "fr-mb-4w")}>
+            Source&nbsp;: {source}
+          </p>
+        </div>
+      </div>
+      <div className={fr.cx("fr-grid-row")}>
+        <div
+          className={fr.cx(
+            "fr-col-12",
+            "fr-col-sm-9",
+            "fr-col-md-8",
+            "fr-col-lg-7",
+            "fr-mb-6w",
+          )}
+        >
+          <Image
+            alt="Image décorative"
+            className={fr.cx("fr-responsive-img", "fr-col-12")}
+            {...image}
+          />
+        </div>
+      </div>
+      <div className={fr.cx("fr-grid-row")}>
+        <div className={fr.cx("fr-col-12", "fr-col-md-10", "fr-col-lg-9")}>
           <MDXRemote
             source={content}
             components={{
