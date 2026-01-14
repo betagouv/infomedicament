31b4e21335dc8f1e5a1c967fc3163964cd1f4b2b
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-01-14 16:47:57 +0100

    db(migrate): update database types
    we should really think about automating this with a tool
    like kysely-codegen...

--- src/db/types.d.ts
+++ src/db/types.d.ts
@@ -22,6 +22,20 @@ export interface Database {
   resume_medicaments: ResumeMedicamentsTable;
   resume_generiques: ResumeGenericsTable;
   letters: LettersTable;
+  ref_articles: RefArticles;
+  ref_atc_friendly_niveau_1: RefAtcFriendlyNiveau1;
+  ref_atc_friendly_niveau_2: RefAtcFriendlyNiveau2;
+  ref_glossaire: RefGlossaire;
+  ref_grossesse_mention: RefGrossesseMention;
+  ref_grossesse_substances_contre_indiquees: RefGrossesseSubstancesContreIndiquees;
+  ref_marr_url_cis: RefMarrUrlCis;
+  ref_marr_url_pdf: RefMarrUrlPdf;
+  ref_pathologies: RefPathologies;
+  ref_pediatrie: RefPediatrie;
+  ref_substance_active: RefSubstanceActive;
+  ref_substance_active_definitions: RefSubstanceActiveDefinitions;
+  atc: Atc;
+  cis_atc: CisAtc;
 }
 
 interface SearchIndexTable {
@@ -148,7 +162,7 @@ interface RatingTable {
 }
 
 export type LetterType = "pathos" | "substances" | "specialites" | "generiques";
-interface LettersTable{
+interface LettersTable {
   type: LetterType;
   letters: string[];
 }
@@ -182,6 +196,108 @@ interface ResumeGenericsTable {
   SpecName: string;
 }
 
+export interface RefArticles {
+  contenu: string | null;
+  homepage: boolean | null;
+  id: Generated<number>;
+  image: string | null;
+  lien: string | null;
+  metadescription: string | null;
+  source: string | null;
+  theme: string | null;
+  titre: string | null;
+}
+
+export interface RefAtcFriendlyNiveau1 {
+  code: string | null;
+  definition_classe: string | null;
+  id: Generated<number>;
+  libelle: string | null;
+}
+
+export interface RefAtcFriendlyNiveau2 {
+  code: string | null;
+  definition_sous_classe: string | null;
+  id: Generated<number>;
+  libelle: string | null;
+}
+
+export interface RefGlossaire {
+  definition: string | null;
+  id: Generated<number>;
+  nom: string | null;
+  source: string | null;
+}
+
+export interface RefGrossesseMention {
+  cis: string | null;
+  id: Generated<number>;
+}
+
+export interface RefGrossesseSubstancesContreIndiquees {
+  id: Generated<number>;
+  lien_site_ansm: string | null;
+  subs_id: string | null;
+}
+
+export interface RefMarrUrlCis {
+  cis: string | null;
+  id: Generated<number>;
+  url: string | null;
+}
+
+export interface RefMarrUrlPdf {
+  id: Generated<number>;
+  nom_document: string | null;
+  type: string | null;
+  url: string | null;
+  url_document: string | null;
+}
+
+export interface RefPathologies {
+  code_patho: string | null;
+  definition: string | null;
+  id: Generated<number>;
+}
+
+export interface RefPediatrie {
+  avis: string | null;
+  cis: string | null;
+  contre_indication: string | null;
+  id: Generated<number>;
+  indication: string | null;
+  mention: string | null;
+}
+
+export interface RefSubstanceActive {
+  definition: string | null;
+  id: Generated<number>;
+  nom_id: string | null;
+  sa: string | null;
+  subs_id: string | null;
+}
+
+export interface RefSubstanceActiveDefinitions {
+  definition: string | null;
+  id: Generated<number>;
+  nom_id: string | null;
+  sa: string | null;
+  subs_id: string | null;
+}
+
+export interface Atc {
+  code: string | null;
+  id: Generated<number>;
+  label: string | null;
+}
+
+export interface CisAtc {
+  code_atc: string | null;
+  code_cis: string | null;
+  id: Generated<number>;
+  label_atc: string | null;
+}
+
 export type LeafletImage = Selectable<LeafletImagesTable>;
 export type SearchResult = Selectable<SearchIndexTable>;
 export type PresentationDetail = Selectable<PresentationTable>;
@@ -195,3 +311,17 @@ export type ResumeSubstance = Selectable<ResumeSubstancesTable>;
 export type ResumeSpecGroupDB = Selectable<ResumeMedicamentsTable>;
 export type ResumeGeneric = Selectable<ResumeGenericsTable>;
 export type Letters = Selectable<LettersTable>;
+export type ATC = Selectable<Atc>;
+export type CIS_ATC = Selectable<CisAtc>;
+export type RefArticles = Selectable<RefArticles>;
+export type RefAtcFriendlyNiveau1 = Selectable<RefAtcFriendlyNiveau1>;
+export type RefAtcFriendlyNiveau2 = Selectable<RefAtcFriendlyNiveau2>;
+export type RefGlossaire = Selectable<RefGlossaire>;
+export type RefGrossesseMention = Selectable<RefGrossesseMention>;
+export type RefGrossesseSubstancesContreIndiquees = Selectable<RefGrossesseSubstancesContreIndiquees>;
+export type RefMarrUrlCis = Selectable<RefMarrUrlCis>;
+export type RefMarrUrlPdf = Selectable<RefMarrUrlPdf>;
+export type RefPathologies = Selectable<RefPathologies>;
+export type RefPediatrie = Selectable<RefPediatrie>;
+export type RefSubstanceActive = Selectable<RefSubstanceActive>;
+export type RefSubstanceActiveDefinitions = Selectable<RefSubstanceActiveDefinitions>;
