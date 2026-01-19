35770393fdef93e48110eb7bab9507a09cb8402c
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-01-19 17:40:14 +0100

    clean(pediatrict): remove dead code

--- src/data/grist/pediatrics.ts
+++ src/data/grist/pediatrics.ts
@@ -12,7 +12,7 @@ export async function getAllPediatrics(): Promise<AllPediatricsInfo[]> {
     "avis",
     "mention",
   ]);
-  
+
   return records.map(({ fields }) => ({
     CIS: fields.CIS.toString().trim(),
     indication: fields.indication.toString().trim() === "oui",
@@ -61,48 +61,4 @@ export async function getPediatrics(
       doctorAdvice: record.fields.avis.trim() === "oui",
       mention: record.fields.mention.trim() === "oui",
     };
-}
-
-export async function getPediatricsForList(
-  CISList: string[],
-): Promise<PediatricsInfo | undefined> {
-  const records = await getGristTableData("Pediatrie", [
-    "CIS",
-    "indication",
-    "contre_indication",
-    "avis",
-    "mention"
-  ]);
-
-  const pediatricsInfo = {
-    indication: false,
-    contraindication: false,
-    doctorAdvice: false,
-    mention: false,
-  }
-  records.forEach(({ fields }) => {
-    if(CISList.includes(fields.CIS.toString().trim())){
-      if (
-        !isOuiOrNon(fields.indication) ||
-        !isOuiOrNon(fields.contre_indication) ||
-        !isOuiOrNon(fields.avis) ||
-        !isOuiOrNon(fields.mention)
-        ) {
-        throw new Error(
-          `Unexpected value in pediatrics data for CIS ${fields.CIS}: ${JSON.stringify(
-            fields,
-          )}`,
-        );
-      }
-      fields.indication.trim() === "oui" && (pediatricsInfo.indication = true);
-      fields.contre_indication.trim() === "oui" && (pediatricsInfo.contraindication = true);
-      fields.avis.trim() === "oui" && (pediatricsInfo.doctorAdvice = true);
-      fields.mention.trim() === "oui" && (pediatricsInfo.mention = true);
-    }
-  });
-
-  if (!pediatricsInfo.indication && !pediatricsInfo.contraindication && !pediatricsInfo.doctorAdvice && !pediatricsInfo.mention) {
-    return;
-  }
-  return pediatricsInfo;
 }
