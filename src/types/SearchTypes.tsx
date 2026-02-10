a1d45471d25ee36ba22d8de451b39e40282979f2
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-02-10 10:41:39 +0100

    refacto(search): remove unused types after search refacto
    Clean up types in SearchTypes.tsx + DataTypes.tsx now that search
    only returns SearchResultItem[]

--- src/types/SearchTypes.tsx
+++ src/types/SearchTypes.tsx
@@ -1,15 +1,6 @@
-import { AdvancedData, DataTypeEnum } from "./DataTypes";
-
-export type ExtendedSearchResults = { [key in DataTypeEnum]: AdvancedData[] };
-
 export type SearchArticlesFilters = {
   ATCList: string[];
   substancesList: string[];
   specialitesList: string[];
   pathologiesList: string[];
 }
-
-export type ExtendedOrderResults = { 
-  counter: number,
-  results: ExtendedSearchResults,
-};
