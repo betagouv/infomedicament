9bba5c8e4ff0c6d7371550bb8b8d78e85328d502
Clemence <technique@clem-dev.com>
2026-02-26 11:28:39 +0100

    fix: reorder specialities header

--- src/components/medicaments/MedicamentContentHeader.tsx
+++ src/components/medicaments/MedicamentContentHeader.tsx
@@ -24,6 +24,50 @@ function MedicamentContentHeader({
 
   return (
     <div {...props}>
+      {(specialite && isAlerteSecurite(specialite)) && (
+        <ContentContainer whiteContainer className={fr.cx("fr-mb-4w")}>
+          <Alert 
+            severity="error"
+            title="Attention ce médicament fait l’objet d’un retrait ou d’une suspension d’autorisation 
+              ou d’utilisation pour des raisons de santé publique."
+            description="Si vous prenez ce médicament, il vous est recommandé de prendre contact, dans les meilleurs délais, 
+              avec votre médecin ou avec votre pharmacien qui vous indiquera la conduite à tenir. 
+              Pour plus d'information, vous pouvez consulter les informations de sécurité ci-dessous."
+            small
+          />
+        </ContentContainer>
+      )}
+      {(ficheInfos && ficheInfos.isSurveillanceRenforcee) && (
+        <ContentContainer whiteContainer className={fr.cx("fr-mb-4w")}>
+          <Alert 
+            severity="warning"
+            title="Médicament sous surveillance renforcée"
+            className={fr.cx("fr-pb-2w", "fr-pt-2w")}
+            description={
+              <div style={{display: "flex"}}>
+                <Image
+                  src="/icons/surveillance_renforcee.png"
+                  alt="Médicament sous surveillance renforcée"
+                  width={21}
+                  height={19}
+                  className={fr.cx("fr-mr-1w", "fr-mt-1v")}
+                />
+                <div>
+                  Ce médicament fait l'objet d'une surveillance renforcée. Pour plus d'informations,&nbsp;
+                  <Link
+                    href="https://ansm.sante.fr/qui-sommes-nous/nos-missions/assurer-la-securite-des-produits-de-sante/p/surveiller-les-medicaments#title"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    cliquez ici
+                  </Link>.
+                </div>
+              </div>
+            }
+            small
+          />
+        </ContentContainer>
+      )}
       {(specialite && !isCommercialisee(specialite)) && (
         <ContentContainer whiteContainer className={fr.cx("fr-mb-4w")}>
           <Alert
@@ -76,50 +120,6 @@ function MedicamentContentHeader({
           L'indication et la posologie sont ainsi adaptées à chaque patient en prenant en 
           compte les données de l'usage traditionnel homéopathique. Ces médicaments peuvent 
           être délivrés par le pharmacien sans prescription médicale. 
-        </ContentContainer>
-      )}
-      {(specialite && isAlerteSecurite(specialite)) && (
-        <ContentContainer whiteContainer className={fr.cx("fr-mb-4w")}>
-          <Alert 
-            severity="error"
-            title="Attention ce médicament fait l’objet d’un retrait ou d’une suspension d’autorisation 
-              ou d’utilisation pour des raisons de santé publique."
-            description="Si vous prenez ce médicament, il vous est recommandé de prendre contact, dans les meilleurs délais, 
-              avec votre médecin ou avec votre pharmacien qui vous indiquera la conduite à tenir. 
-              Pour plus d'information, vous pouvez consulter les informations de sécurité ci-dessous."
-            small
-          />
-        </ContentContainer>
-      )}
-      {(ficheInfos && ficheInfos.isSurveillanceRenforcee) && (
-        <ContentContainer whiteContainer className={fr.cx("fr-mb-4w")}>
-          <Alert 
-            severity="warning"
-            title="Médicament sous surveillance renforcée"
-            className={fr.cx("fr-pb-2w", "fr-pt-2w")}
-            description={
-              <div style={{display: "flex"}}>
-                <Image
-                  src="/icons/surveillance_renforcee.png"
-                  alt="Médicament sous surveillance renforcée"
-                  width={21}
-                  height={19}
-                  className={fr.cx("fr-mr-1w", "fr-mt-1v")}
-                />
-                <div>
-                  Ce médicament fait l'objet d'une surveillance renforcée. Pour plus d'informations,&nbsp;
-                  <Link
-                    href="https://ansm.sante.fr/qui-sommes-nous/nos-missions/assurer-la-securite-des-produits-de-sante/p/surveiller-les-medicaments#title"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    cliquez ici
-                  </Link>.
-                </div>
-              </div>
-            }
-            small
-          />
         </ContentContainer>
       )}
     </div>
