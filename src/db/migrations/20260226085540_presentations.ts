e5d81a40a67af8de834e7d0fa22737ab75371b7e
Clemence <technique@clem-dev.com>
2026-02-26 12:56:31 +0100

    feat: dispositif for all presentation not just recipient

--- src/db/migrations/20260226085540_presentations.ts
+++ src/db/migrations/20260226085540_presentations.ts
@@ -3,13 +3,13 @@ import type { Kysely } from 'kysely'
 export async function up(db: Kysely<any>): Promise<void> {
 	await db.schema
 		.alterTable('presentations')
-		.alterColumn('nbrrecipient', (col) => col.setDataType("double precision"))
+		.alterColumn('qtecontenance', (col) => col.setDataType("double precision"))
 		.execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
 	await db.schema
 		.alterTable('presentations')
-		.alterColumn('nbrrecipient', (col) => col.setDataType("integer"))
+		.alterColumn('qtecontenance', (col) => col.setDataType("integer"))
 		.execute();
 }
