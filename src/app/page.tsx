6ba5727ae4d1484a8d387792dca0fb19d9b7fdfd
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-03-06 12:15:42 +0100

    fix: remove RatingToaster from homepage
    its too early to ask users if the site is useful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 import { fr } from "@codegouvfr/react-dsfr";
 import AutocompleteSearch from "@/components/AutocompleteSearch";
 import { getArticles } from "@/db/utils/articles";
-import RatingToaster from "@/components/rating/RatingToaster";
 import ArticlesSimpleList from "@/components/articles/ArticlesSimpleList";
 import { Article } from "@/types/ArticlesTypes";
-
-const PAGE_LABEL: string = "Accueil";
 
 export default async function Page() {
   const articles: Article[] = (await getArticles()).filter(({ homepage }) => homepage);
@@ -87,9 +84,6 @@ export default async function Page() {
           </div>
         </div>
       </div>
-      <RatingToaster
-        pageId={PAGE_LABEL}
-      />
     </>
   );
 }
