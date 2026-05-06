51f2b33ad295a57b93777ec3b5e884470f93ea17
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-05-06 15:12:04 +0200

    refactor: remove keyword-highlight CSS from NoticeBlock
      Dynamic .highlight-* rules are no longer needed now that question navigation
      uses section anchors instead of DOM class toggling.

--- src/components/medicaments/notice/NoticeBlock.tsx
+++ src/components/medicaments/notice/NoticeBlock.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { HTMLAttributes } from "react";
-import styled, { css } from 'styled-components';
-import { questionsList, questionKeys } from "@/data/pages/notices_anchors";
-import { QuestionsListFormat } from "@/types/NoticesAnchors";
+import styled from 'styled-components';
 import { fr } from "@codegouvfr/react-dsfr";
 import ContentContainer from "@/components/generic/ContentContainer";
 import { RcpNoticeContainer } from "../blocks/GenericBlocks";
@@ -14,29 +12,7 @@ import { Definition } from "@/types/GlossaireTypes";
 import { isCentralisee } from "@/utils/specialites";
 import CentraliseBlock from "../blocks/CentraliseBlock";
 
-const NoticeBlockContainer = styled.div<{ $questionsList: QuestionsListFormat; $questionKeys: string[] }> `
-  ${props => props.$questionKeys.map(key => {
-  //First time for header
-  return props.$questionsList[key].id && (
-    css`
-      .highlight-${key} .highlight-keyword-${props.$questionsList[key].id} {
-        background-color: var(--green-tilleul-verveine-950-100);
-      }`
-  )
-  })};
-  ${props => props.$questionKeys.map(key => {
-    //Second time for keywords
-    if (props.$questionsList[key].keywords) {
-      return css`
-        .highlight-${key} .highlight-keyword-${key} {
-          background-color: var(--green-tilleul-verveine-950-100);
-        }
-        .highlight-${key} .highlight-keyword-${key}.active{
-          background-color: orange;
-        }`;
-    }
-  })};
-`;
+const NoticeBlockContainer = styled.div``;
 
 interface NoticeBlockProps extends HTMLAttributes<HTMLDivElement> {
   notice?: NoticeData,
@@ -54,9 +30,7 @@ function NoticeBlock({
 }: NoticeBlockProps) {
 
   return (
-    <NoticeBlockContainer 
-      $questionsList={questionsList} 
-      $questionKeys={questionKeys} 
+    <NoticeBlockContainer
       {...props}
       className={[props.className, fr.cx("fr-mt-3w")].join(" ")}
     >
