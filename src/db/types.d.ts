463f012ac6632e42f3ba13b22f066fad9779b4db
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-05-27 15:29:58 +0200

    feat(migration): add bdpm_caracteristique table

--- src/db/types.d.ts
+++ src/db/types.d.ts
@@ -58,6 +58,7 @@ export interface Database {
   bdpm_recipient: BdpmRecipientTable;
   bdpm_dispositif: BdpmDispositifTable;
   bdpm_document: BdpmDocumentTable;
+  bdpm_caracteristique: BdpmCaracteristiqueTable;
 }
 
 interface SearchIndexTable {
@@ -628,3 +629,13 @@ interface BdpmDocumentTable {
 }
 
 export type BdpmDocument = Selectable<BdpmDocumentTable>;
+
+interface BdpmCaracteristiqueTable {
+  cip: string;
+  numero_recipient: number;
+  code_caracteristique: number;
+  ordre: number | null;
+  libelle: string | null;
+}
+
+export type BdpmCaracteristique = Selectable<BdpmCaracteristiqueTable>;
