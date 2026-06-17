26b6c8daab593856a304c41b117fa4cac2cee4de
Vincent Viers <vincent.viers@beta.gouv.fr>
2026-06-17 15:23:13 +0200

    Revert "feat: add tests"
    This reverts commit b219200e1c46879ba956b69d0c34d9ae1a29fc36.

--- src/db/utils/generics.integration.test.ts
+++ src/db/utils/generics.integration.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { getGeneriques, getGroupeGene, getIsPrinceps } from "./generics";
+import { getGeneriques } from "./generics";
 
 describe("db utils generics", () => {
 
@@ -14,28 +14,5 @@ describe("db utils generics", () => {
 
     expect(isInactiveSpec).toBe(-1);
     expect(isActiveSpec).not.toBe(-1);
-  });
-
-  it("getGroupeGene - should return only actives generiques", async () => {
-    //Tramadol (CHLORHYDRATE De) 37,5 mg + Paracétamol 325 mg
-    const groups = await getGroupeGene("60327615");
-    expect(groups.length).toBe(2);
-
-    //Abacavir (SULFATE D') équivalant à Abacavir 300 mg
-    const groups2 = await getGroupeGene("68556562");
-    expect(groups2.length).toBe(1);
-  });
-
-  it("getIsPrinceps", async () => {
-    //All the tests are for the same group : Tramadol (CHLORHYDRATE De) 37,5 mg + Paracétamol 325 mg
-    const isPrinceps = await getIsPrinceps("60327615");
-    expect(isPrinceps).toBe(true);
-    const isPrinceps2 = await getIsPrinceps("61137241");
-    expect(isPrinceps2).toBe(true);
-    const isPrinceps3 = await getIsPrinceps("63561242");
-    expect(isPrinceps3).toBe(false);
-    const isPrinceps4 = await getIsPrinceps("69925400");
-    expect(isPrinceps4).toBe(false);
-  });
-
+  })
 });
